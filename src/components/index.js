5564f0d75405a02551c25b9c29bbb51ce24f0cb7
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-06-05 23:49:10 +0800

    ProductDetails

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,6 @@
+export { Rating } from "./Elements/Rating";
+
 export { Header } from "./Layouts/Header";
 export { Footer } from "./Layouts/Footer";
+
+export { ProductCard } from "./Elements/ProductCard";
