778744a1bdf94937503d32ecab2d431e07a83751
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-06-08 23:52:00 +0800

    Filter properties

--- src/context/FilterContext.js
+++ src/context/FilterContext.js
@@ -23,7 +23,52 @@ export const FilterProvider = ({ children }) => {
     });
   }
 
+  //functions
+  function bestSeller(products) {
+    return state.bestSellerOnly
+      ? products.filter((product) => product.best_seller === true)
+      : products;
+  }
+
+  function inStock(products) {
+    return state.onlyInStock
+      ? products.filter((product) => product.in_stock === true)
+      : products;
+  }
+
+  function sort(products) {
+    if (state.sortBy === "lowtohigh") {
+      return products.sort((a, b) => Number(a.price) - Number(b.price));
+    }
+    if (state.sortBy === "hightolow") {
+      return products.sort((a, b) => Number(b.price) - Number(a.price));
+    }
+    return products;
+  }
+
+  function rating(products) {
+    if (state.ratings === "4STARSABOVE") {
+      return products.filter((product) => product.rating >= 4);
+    }
+    if (state.ratings === "3STARSABOVE") {
+      return products.filter((product) => product.rating >= 3);
+    }
+    if (state.ratings === "2STARSABOVE") {
+      return products.filter((product) => product.rating >= 2);
+    }
+    if (state.ratings === "1STARSABOVE") {
+      return products.filter((product) => product.rating >= 1);
+    }
+    return products;
+  }
+
+  const filteredProductList = rating(
+    sort(inStock(bestSeller(state.productList)))
+  );
+  //end functions
   const value = {
+    state,
+    dispatch,
     products: state.productList,
     initialProductList,
   };
@@ -33,4 +78,7 @@ export const FilterProvider = ({ children }) => {
   );
 };
 
-export const useFilter = () => useContext(FilterContext);
+export const useFilter = () => {
+  const context = useContext(FilterContext);
+  return context;
+};
