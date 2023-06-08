9352f7c1ff3d17a6899662f21086d1774921fac6
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-06-09 00:44:31 +0800

    Fixed filter properties

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { AllRoutes } from "./routes/AllRoutes";
-import { Footer, Header } from "./components";
+import { AllRoutes } from './routes/AllRoutes';
+import { Footer, Header } from './components';
 
 function App() {
   return (
-    <div className="App dark:bg-slate-800">
+    <div className="App dark:bg-dark">
       <Header />
       <AllRoutes />
       <Footer />
--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
-export { Rating } from "./Elements/Rating";
-
 export { Header } from "./Layouts/Header";
 export { Footer } from "./Layouts/Footer";
 
 export { ProductCard } from "./Elements/ProductCard";
+export { Rating } from "./Elements/Rating";
+
 export { ScrollToTop } from "./Other/ScrollToTop";
