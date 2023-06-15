299e1db003db1526a7cc6bbc5724e1bc03abde3e
eymmProjects <64175791+eymmProjects@users.noreply.github.com>
2023-06-15 21:40:30 +0800

    fix rename issue

--- src/pages/Cart/CartPage.js
+++ src/pages/Cart/CartPage.js
@@ -4,5 +4,9 @@ import { CartList } from "./components/CartList";
 export const CartPage = () => {
   const cartlistLength = 0;
 
-  return <main>{cartlistLength ? <CartList /> : <CartEmpty />}</main>;
-};
+  return (
+    <main>       
+      { cartlistLength ? <CartList /> : <CartEmpty /> }   
+    </main>
+  )
+}
--- src/routes/AllRoutes.js
+++ src/routes/AllRoutes.js
@@ -1,24 +1,19 @@
 import { Routes, Route } from "react-router-dom";
-import {
-  HomePage,
-  ProductsList,
-  ProductDetail,
-  Login,
-  Register,
-  CartPage,
-} from "../pages";
+import { HomePage, ProductsList, ProductDetail, Login, Register, CartPage } from "../pages";
 
 export const AllRoutes = () => {
   return (
     <>
-      <Routes>
+    <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/products" element={<ProductsList />} />
-        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="products" element={<ProductsList />} />
+        <Route path="products/:id" element={<ProductDetail />} />
+
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
+
         <Route path="cart" element={<CartPage />} />
-      </Routes>
+    </Routes>
     </>
-  );
-};
+  )
+}
